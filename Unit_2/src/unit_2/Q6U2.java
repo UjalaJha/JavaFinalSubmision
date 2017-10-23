3661141421df168165315fe015924fcc79a8e467
UjalaJha <UjalaJha>
2017-10-23 23:02:08 +0600

    java:))

--- Unit_2/src/unit_2/Q6U2.java
+++ Unit_2/src/unit_2/Q6U2.java
@@ -13,39 +13,34 @@ import java.util.Vector;
  * @author prakash
  */
 public class Q6U2 {
- public static void main(String[] args) 
-     {
-         Scanner sc=new Scanner(System.in);
-         Vector v= new Vector(5,1); 
-         System.out.println("Enter Vector ");
-         v.addElement(1);
-         v.addElement(2);
-         v.addElement(3);
-         v.addElement(5);
-         v.addElement(1);
-         v.addElement(1);
-         v.addElement(1);
-         
-         for(Object i:v)
-         System.out.println(i+" ");
-         
-         int c=0;
-         System.out.println("Enter the element to check frequency");
-         int num=sc.nextInt();
-         
-         int n=v.capacity();
-         Integer[] a = new Integer[n];
-         v.toArray(a);
-         
-        for (int i = 0; i < n; i++)
+
+ public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc=new Scanner(System.in);
+        Vector<String> v=new Vector<String>(20,10);
+        String s=new String();
+        int count=0;
+        String element;
+        System.out.println("Enter the number of Strings: ");
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
         {
-            if(a[i]==num)
-            {
-                c++;
-            }
+             System.out.println("Enter the String to be inserted: ");
+             s=sc.next();
+             v.addElement(s);
         }
-        
-        System.out.println("frequency = "+c);
-     }
+        System.out.println("Entered vector is: ");
+        for(Object i:v)
+        System.out.println(i+" ");
+         
+        System.out.println("Enter the Element to be Searched: ");
+        element=sc.next();
+       for(int i=0;i<n;i++)
+       {
+           if(element.equals(v.elementAt(i)))
+           {
+               count++; }}
+       System.out.println("The Frequency of the Element is: "+count);
+    }
 }
 
